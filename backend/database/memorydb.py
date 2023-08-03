0ce2e302b12efb17208c45f9a0c48e41e6b74f6a
Frederic Vogels <frederic.vogels@ucll.be>
2023-08-03 10:00:25 +0200

    updates to database package

--- backend/database/memorydb.py
+++ backend/database/memorydb.py
@@ -3,8 +3,19 @@ import backend.database.models as models
 
 
 class InMemoryDatabase(Database):
-    def list_items(self):
-        return [
-            models.Item(description='some item', price_in_cents=100),
-            models.Item(description='some other item', price_in_cents=200),
+    __items: list[models.ItemCreate]
+
+    def __init__(self):
+        self.__items = [
+            models.Item(item_id=1, description='some item', price_in_cents=100),
+            models.Item(item_id=2, description='some other item', price_in_cents=200),
         ]
+
+    def list_items(self) -> list[models.Item]:
+        return self.__items
+
+    def create_item(self, item: models.ItemCreate) -> int:
+        id = self.__generate_unique_item_id()
+        new_item = models.Item(**item, id=id)
+        self.__items.append(new_item)
+        return id
--- backend/database/models.py
+++ backend/database/models.py
@@ -1,6 +1,27 @@
 import pydantic
+import datetime
 
 
-class Item(pydantic.BaseModel):
+class SaleBase(pydantic.BaseModel):
+    date: datetime.date
+
+
+class SaleCreate(SaleBase):
+    pass
+
+
+class Sale(SaleBase):
+    sale_id: int
+
+
+class ItemBase(pydantic.BaseModel):
     description: str
     price_in_cents: pydantic.NonNegativeInt
+
+
+class ItemCreate(ItemBase):
+    pass
+
+
+class Item(ItemBase):
+    item_id: int
