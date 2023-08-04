42417ff6dc775d5c2b69495bcfd7426d537b615e
Frederic Vogels <frederic.vogels@ucll.be>
2023-08-04 10:54:29 +0200

    token tests

---
+++ src/tests/token-tests.py
@@ -0,0 +1,25 @@
+import pytest
+from backend.security import scopes
+from backend.security.tokens import create_access_token, decode_access_token, TokenData
+from datetime import timedelta
+
+
+@pytest.mark.parametrize('email_address', [
+    'test@gmail.com',
+    'foo@domain.org',
+])
+@pytest.mark.parametrize('scopes', [
+    set(),
+    {
+        scopes.LIST_ACCOUNTS
+    },
+])
+def test_access_token(email_address: str, scopes: set[scopes.Scope]):
+    token_data = TokenData(email_address=email_address, scopes=scopes)
+    duration = timedelta(hours=1)
+    access_token = create_access_token(token_data=token_data, duration=duration)
+    decoded_token_data = decode_access_token(access_token)
+
+    assert decoded_token_data is not None
+    assert decoded_token_data.email_address == email_address
+    assert decoded_token_data.scopes == scopes
