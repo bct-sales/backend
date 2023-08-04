f0c5a6078d15c63f68a0ce5ee4173d367baf19ee
Frederic Vogels <frederic.vogels@ucll.be>
2023-08-04 10:53:55 +0200

    optional database path in settings

--- src/backend/settings.py
+++ src/backend/settings.py
@@ -6,12 +6,21 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_prefix='BCT_')
 
-    database_path: str
+    database_path: Optional[str] = None
+
+    jwt_secret_key: str = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+
+    jwt_algorithm: str = 'HS256'
+
+    jwt_expiration: int = 30
 
     @pydantic.computed_field
     @property
     def database_url(self) -> str:
-        return f'sqlite:///{self.database_path}'
+        if self.database_path:
+            return f'sqlite:///{self.database_path}'
+        else:
+            return 'sqlite:///'
 
 
 _settings: Optional[Settings] = None
