52334884bbfbe41240ecb7e1d3e9daeb9c8bc719
Frederic Vogels <frederic.vogels@ucll.be>
2023-08-04 10:53:42 +0200

    updated createdb.py

--- src/createdb.py
+++ src/createdb.py
@@ -7,9 +7,13 @@ import os
 
 settings = load_settings()
 
-if os.path.isfile(settings.database_path):
-    logging.error('Database already exists')
+if (database_path := settings.database_path) is None:
+    logging.error('No database path set; use BCT_DATABASE_PATH environment variable')
     sys.exit(-1)
+
+if os.path.isfile(database_path):
+    logging.error('Database already exists')
+    sys.exit(-2)
 
 database = Database(url=settings.database_url)
 
