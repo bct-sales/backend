64c91ab903797ad6b62252ad7bf0ec28826e2198
Frederic Vogels <frederic.vogels@ucll.be>
2023-08-06 15:43:15 +0200

    cleaned up imports

--- src/tests/rest/login-tests.py
+++ src/tests/rest/login-tests.py
@@ -2,9 +2,7 @@ import pytest
 from fastapi import status
 from fastapi.testclient import TestClient
 
-from backend.database import models
 from backend.database.base import DatabaseSession
-from backend.security import roles
 from tests.conftest import Seller
 
 
