39370d7c7246df6aa13ea854d1146cbdfc72ee98
Frederic Vogels <frederic.vogels@ucll.be>
2023-08-06 17:45:55 +0200

    create event route

--- src/backend/restapi/events.py
+++ src/backend/restapi/events.py
@@ -17,3 +17,12 @@ def get_sales_events(database: DatabaseDependency,
                     ))]):
     orm_sales_events = database.list_sales_events()
     return orm_sales_events
+
+
+@router.post('/', response_model=list[models.SalesEvent])
+def add_sales_event(event_data: models.SalesEventCreate,
+                    database: DatabaseDependency,
+                    user: Annotated[orm.User, RequireScopes(scopes.Scopes(
+                       scopes.ADD_SALES_EVENTS,
+                   ))]):
+    database.create_sales_event(event_data)
