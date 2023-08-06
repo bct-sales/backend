8c24c46eb4307a84397f87fe16542bd30c1f0e85
Frederic Vogels <frederic.vogels@ucll.be>
2023-08-06 22:54:34 +0200

    test list events as admin

--- src/tests/rest/rest-event-tests.py
+++ src/tests/rest/rest-event-tests.py
@@ -52,4 +52,14 @@ def test_list_events_as_admin(client: TestClient,
                               sales_event: models.SalesEventCreate):
     headers = {'Authorization': f'Bearer {admin_access_token}'}
     response = client.get('/events', headers=headers)
+    json = response.json()
+
     assert response.status_code == status.HTTP_200_OK
+    assert len(json) == 1
+    assert len(json[0]) == 6
+    assert 'sales_event_id' in json[0]
+    assert json[0]['date'] == sales_event.date.isoformat()
+    assert json[0]['description'] == sales_event.description
+    assert json[0]['start_time'] == sales_event.start_time.isoformat()
+    assert json[0]['end_time'] == sales_event.end_time.isoformat()
+    assert json[0]['location'] == sales_event.location
