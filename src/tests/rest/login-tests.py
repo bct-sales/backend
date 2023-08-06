c631cdc01148a197e4d44a17faa4608619fb925b
Frederic Vogels <frederic.vogels@ucll.be>
2023-08-06 15:39:52 +0200

    added test logging in with nonexistent email account

--- src/tests/rest/login-tests.py
+++ src/tests/rest/login-tests.py
@@ -22,3 +22,18 @@ def test_login(client: TestClient, session: DatabaseSession, seller: Seller):
 
     assert response.status_code == status.HTTP_200_OK
 
+
+def test_login_with_nonexisting_email_address(client: TestClient, session: DatabaseSession, valid_email_address: str, valid_password):
+    payload = {
+        'grant_type': 'password',
+        'username': valid_email_address,
+        'password': valid_password,
+    }
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+
+    response = client.post('/login', data=payload, headers=headers)
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
