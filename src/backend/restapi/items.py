fd2c10726657254eb05ad99451be40b1c23e1097
Frederic Vogels <frederic.vogels@ucll.be>
2023-08-06 15:01:40 +0200

    removed redundant code

--- src/backend/restapi/items.py
+++ src/backend/restapi/items.py
@@ -18,21 +18,6 @@ class _Item(pydantic.BaseModel):
     price_in_cents: pydantic.NonNegativeInt
 
 
-class User(pydantic.BaseModel):
-    id: str
-    scopes: list[str]
-
-def get_user() -> User:
-    return User(id='jos', scopes=['a', 'b', 'c'])
-
-def RequiresScopes(*scopes: str) -> Any:
-    def dependency(user: User = Depends(get_user)):
-        if not set(scopes) <= set(user.scopes):
-            raise HTTPException(status.HTTP_401_UNAUTHORIZED)
-        return user
-    return Depends(dependency)
-
-
 @router.get("/items", response_model=list[_Item])
 async def list_items(database: DatabaseDependency):
     return database.list_items()
