a08f117cfce951bf4b12b2ca3423ffda7d67cbf3
Frederic Vogels <frederic.vogels@ucll.be>
2023-08-07 09:46:52 +0200

    renamed tests

--- src/tests/rest/rest-user-tests.py
+++ src/tests/rest/rest-user-tests.py
@@ -6,27 +6,27 @@ from backend.db.database import DatabaseSession
 from backend.db import models
 
 
-def test_list_events_not_logged_in(client: TestClient,
+def test_list_items_not_logged_in(client: TestClient,
                                    session: DatabaseSession,
                                    sales_event: models.SalesEventCreate):
-    response = client.get('/events')
+    response = client.get('/me/items')
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
 
-def test_list_events_as_seller(client: TestClient,
-                               session: DatabaseSession,
-                               seller_headers: dict[str, str],
-                               sales_event: models.SalesEventCreate):
+def test_list_items_as_seller(client: TestClient,
+                              session: DatabaseSession,
+                              seller_headers: dict[str, str],
+                              sales_event: models.SalesEventCreate):
     response = client.get('/me/items', headers=seller_headers)
     json = response.json()
 
     assert response.status_code == status.HTTP_200_OK
 
 
-def test_list_events_as_admin(client: TestClient,
-                              session: DatabaseSession,
-                              admin_headers: dict[str, str],
-                              sales_event: models.SalesEventCreate):
+def test_list_items_as_admin(client: TestClient,
+                             session: DatabaseSession,
+                             admin_headers: dict[str, str],
+                             sales_event: models.SalesEventCreate):
     response = client.get('/me/items', headers=admin_headers)
     json = response.json()
 
