c5eef27e10860dbd320ad53a22f8fe3d1592fcce
Frederic Vogels <frederic.vogels@ucll.be>
2023-08-11 17:27:54 +0200

    update sales event route

--- src/backend/restapi/events.py
+++ src/backend/restapi/events.py
@@ -1,6 +1,7 @@
-from typing import Annotated
+from typing import Annotated, Optional
 
 from fastapi import APIRouter
+import pydantic
 
 from backend.db import models
 from backend.db.exceptions import *
@@ -25,3 +26,25 @@ def add_sales_event(event_data: models.SalesEventCreate,
                     user: Annotated[orm.User, RequireScopes(scopes.Scopes(scopes.ADD_SALES_EVENTS))]):
     orm_sales_event = database.create_sales_event(event_data)
     return models.SalesEvent.model_validate(orm_sales_event)
+
+
+
+class _UpdateSalesPayload(pydantic.BaseModel):
+    date: Optional[str] = None
+    start_time: Optional[str] = None
+    end_time: Optional[str] = None
+    location: Optional[str] = None
+    description: Optional[str] = None
+
+
+@router.put('/{sales_event_id}')
+def update_sales_event(database: DatabaseDependency,
+                       payload: _UpdateSalesPayload,
+                       sales_event_id: int,
+                       user: Annotated[orm.User, RequireScopes(scopes.Scopes(scopes.EDIT_SALES_EVENT))]):
+    orm_sales_event = database.find_sales_event_by_id(sales_event_id)
+    if orm_sales_event is None:
+        raise HTTPException(status.HTTP_404_NOT_FOUND)
+    for field, value in payload:
+        setattr(orm_sales_event, field, value)
+    database.commit()
