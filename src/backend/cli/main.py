d4d0af0ef357c5d3b73ea11bf697259df8f608c7
Frederic Vogels <frederic.vogels@ucll.be>
2023-08-12 21:06:52 +0200

    put dev cli command in separate file

--- src/backend/cli/main.py
+++ src/backend/cli/main.py
@@ -2,9 +2,10 @@ from backend.cli.database import get_database
 from backend.db import models
 from backend.security import roles
 from backend.security.tokens import create_access_token, TokenData
+from backend.cli.dev import dev
 import datetime
 import click
-import sys
+
 
 
 @click.group()
@@ -82,28 +83,4 @@ def repopulate():
             owner_id=seller.user_id))
 
 
-@cli.group
-def dev():
-    pass
-
-
-@dev.command
-@click.argument('email')
-def genauth(email):
-    database = get_database()
-    with database.session as session:
-        user = session.find_user_with_email_address(email_address=email)
-
-    if user is None:
-        print(f'Error: no user found with email address {user}')
-        sys.exit(-1)
-
-    role = roles.Role.from_name(user.role)
-    token_data = TokenData(user_id=user.user_id, scopes=role.scopes)
-    access_token = create_access_token(token_data=token_data, duration=datetime.timedelta(days=365))
-    print(f'''
-const accessToken = `{access_token}`;
-const emailAddress = `{email}`;
-const role = '{role.name}';
-const userId = {user.user_id};
-          '''.strip())
+cli.add_command(dev)
