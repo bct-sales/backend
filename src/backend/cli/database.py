bd92e49437f0b527feae18064f16e5bfb73c3449
Frederic Vogels <frederic.vogels@ucll.be>
2023-08-12 21:08:53 +0200

    db subcommands in separate file

--- src/backend/cli/database.py
+++ src/backend/cli/database.py
@@ -1,7 +1,13 @@
-from backend.db.database import Database
-from backend.settings import load_settings
+import datetime
 import logging
 import sys
+
+import click
+
+from backend.db import models
+from backend.db.database import Database
+from backend.security import roles
+from backend.settings import load_settings
 
 
 def get_database() -> Database:
@@ -12,3 +18,73 @@ def get_database() -> Database:
         sys.exit(-1)
 
     return Database(name="Production Database", url=settings.database_url)
+
+
+@click.group('db')
+def db():
+    pass
+
+
+@db.command(help="Removes ALL data from database")
+def reset():
+    database = get_database()
+    database.drop_tables()
+    database.create_tables()
+
+
+@db.command(help="Removes ALL data and adds dummy seller and admin")
+def repopulate():
+    database = get_database()
+    database.drop_tables()
+    database.create_tables()
+    with database.session as session:
+        seller = session.create_user(models.UserCreate(
+            email_address='seller@bct.be',
+            role=roles.SELLER.name,
+            password='123456789'
+        ))
+        session.create_user(models.UserCreate(
+            email_address='admin@bct.be',
+            role=roles.ADMIN.name,
+            password='123456789'
+        ))
+        event = session.create_sales_event(models.SalesEventCreate(
+            date=datetime.date(2023, 12, 18),
+            start_time=datetime.time(9, 0),
+            end_time=datetime.time(18, 0),
+            location='Leuven',
+            description='Leuven Sales'
+        ))
+        session.create_sales_event(models.SalesEventCreate(
+            date=datetime.date(2040, 1, 1),
+            start_time=datetime.time(10, 0),
+            end_time=datetime.time(12, 0),
+            location='Brussels',
+            description='Brussels Sales'
+        ))
+        session.create_sales_event(models.SalesEventCreate(
+            date=datetime.date(2050, 1, 1),
+            start_time=datetime.time(10, 0),
+            end_time=datetime.time(12, 0),
+            location='Antwerp',
+            description='Antwerp Sales'
+        ))
+        session.create_sales_event(models.SalesEventCreate(
+            date=datetime.date(2060, 1, 1),
+            start_time=datetime.time(10, 0),
+            end_time=datetime.time(12, 0),
+            location='Ghent',
+            description='Ghent Sales'
+        ))
+        session.create_item(item=models.ItemCreate(
+            description='T-Shirt',
+            price_in_cents=200,
+            recipient_id=seller.user_id,
+            sales_event_id=event.sales_event_id,
+            owner_id=seller.user_id))
+        session.create_item(item=models.ItemCreate(
+            description='Jeans',
+            price_in_cents=800,
+            recipient_id=seller.user_id,
+            sales_event_id=event.sales_event_id,
+            owner_id=seller.user_id))
--- src/backend/cli/main.py
+++ src/backend/cli/main.py
@@ -3,9 +3,9 @@ from backend.db import models
 from backend.security import roles
 from backend.security.tokens import create_access_token, TokenData
 from backend.cli.dev import dev
+from backend.cli.database import db
 import datetime
 import click
-
 
 
 @click.group()
@@ -13,74 +13,5 @@ def cli():
     pass
 
 
-@cli.group('db')
-def db():
-    pass
-
-
-@db.command(help="Removes ALL data from database")
-def reset():
-    database = get_database()
-    database.drop_tables()
-    database.create_tables()
-
-
-@db.command(help="Removes ALL data and adds dummy seller and admin")
-def repopulate():
-    database = get_database()
-    database.drop_tables()
-    database.create_tables()
-    with database.session as session:
-        seller = session.create_user(models.UserCreate(
-            email_address='seller@bct.be',
-            role=roles.SELLER.name,
-            password='123456789'
-        ))
-        session.create_user(models.UserCreate(
-            email_address='admin@bct.be',
-            role=roles.ADMIN.name,
-            password='123456789'
-        ))
-        event = session.create_sales_event(models.SalesEventCreate(
-            date=datetime.date(2023, 12, 18),
-            start_time=datetime.time(9, 0),
-            end_time=datetime.time(18, 0),
-            location='Leuven',
-            description='Leuven Sales'
-        ))
-        session.create_sales_event(models.SalesEventCreate(
-            date=datetime.date(2040, 1, 1),
-            start_time=datetime.time(10, 0),
-            end_time=datetime.time(12, 0),
-            location='Brussels',
-            description='Brussels Sales'
-        ))
-        session.create_sales_event(models.SalesEventCreate(
-            date=datetime.date(2050, 1, 1),
-            start_time=datetime.time(10, 0),
-            end_time=datetime.time(12, 0),
-            location='Antwerp',
-            description='Antwerp Sales'
-        ))
-        session.create_sales_event(models.SalesEventCreate(
-            date=datetime.date(2060, 1, 1),
-            start_time=datetime.time(10, 0),
-            end_time=datetime.time(12, 0),
-            location='Ghent',
-            description='Ghent Sales'
-        ))
-        session.create_item(item=models.ItemCreate(
-            description='T-Shirt',
-            price_in_cents=200,
-            recipient_id=seller.user_id,
-            sales_event_id=event.sales_event_id,
-            owner_id=seller.user_id))
-        session.create_item(item=models.ItemCreate(
-            description='Jeans',
-            price_in_cents=800,
-            recipient_id=seller.user_id,
-            sales_event_id=event.sales_event_id,
-            owner_id=seller.user_id))
-
-
+cli.add_command(db)
 cli.add_command(dev)
