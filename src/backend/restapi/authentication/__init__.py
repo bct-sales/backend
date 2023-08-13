22c884c1bf6d089bc6595a3b6e694be01b1f05ea
Frederic Vogels <frederic.vogels@ucll.be>
2023-08-13 11:38:59 +0200

    registration -> login

--- src/backend/restapi/authentication/__init__.py
+++ src/backend/restapi/authentication/__init__.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
-from . import authentication
+from . import login
 from . import register
 
 
 router = APIRouter()
 
-router.include_router(authentication.router)
+router.include_router(login.router)
 router.include_router(register.router)
