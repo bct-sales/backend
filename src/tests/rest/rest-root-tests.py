9a6666ea71acf7069780ec08fc8cbba190dfaa54
Frederic Vogels <frederic.vogels@ucll.be>
2023-08-15 12:17:55 +0200

    root tests

---
+++ src/tests/rest/rest-root-tests.py
@@ -0,0 +1,17 @@
+import pytest
+from fastapi import status
+from fastapi.testclient import TestClient
+
+from tests.util import Exists
+
+
+def test_root(client: TestClient):
+    response = client.get('/api/v1')
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {
+        'links': {
+            'registration': Exists(),
+            'login': Exists(),
+            'events': Exists(),
+        }
+    }
