1b5a5ff3057dcac54a47122cb7a24bd0dc4ed7d1
Frederic Vogels <frederic.vogels@ucll.be>
2023-08-20 18:06:08 +0200

    db tests

--- src/tests/database/db-item-tests.py
+++ src/tests/database/db-item-tests.py
@@ -44,11 +44,11 @@ def test_create_item(session: DatabaseSession,
     50,
     1200
 ])
-def test_list_items(session: DatabaseSession,
-                    seller: models.User,
-                    sales_event: models.SalesEvent,
-                    description: str,
-                    price_in_cents: int):
+def test_find_item(session: DatabaseSession,
+                   seller: models.User,
+                   sales_event: models.SalesEvent,
+                   description: str,
+                   price_in_cents: int):
     item_creation = models.ItemCreate(
         description=description,
         price_in_cents=price_in_cents,
@@ -65,3 +65,68 @@ def test_list_items(session: DatabaseSession,
     assert item.owner_id == seller.user_id
     assert item.recipient_id == seller.user_id
     assert item.price_in_cents == price_in_cents
+
+
+@pytest.mark.parametrize('description', [
+    'blue shirt',
+    'black shirt',
+    'white hat',
+])
+@pytest.mark.parametrize('price_in_cents', [
+    50,
+    1200
+])
+def test_list_items(session: DatabaseSession,
+                    seller: models.User,
+                    sales_event: models.SalesEvent,
+                    description: str,
+                    price_in_cents: int):
+    item_creation = models.ItemCreate(
+        description=description,
+        price_in_cents=price_in_cents,
+        owner_id=seller.user_id,
+        recipient_id=seller.user_id,
+        sales_event_id=sales_event.sales_event_id,
+    )
+
+    orm_item = session.create_item(item=item_creation)
+    items = session.list_items()
+
+    assert len(items) == 1
+    item = items[0]
+
+    assert item is not None
+    assert item.description == description
+    assert item.owner_id == seller.user_id
+    assert item.recipient_id == seller.user_id
+    assert item.price_in_cents == price_in_cents
+
+
+@pytest.mark.parametrize('description', [
+    'blue shirt',
+    'black shirt',
+    'white hat',
+])
+@pytest.mark.parametrize('price_in_cents', [
+    50,
+    1200
+])
+def test_delete_item(session: DatabaseSession,
+                    seller: models.User,
+                    sales_event: models.SalesEvent,
+                    description: str,
+                    price_in_cents: int):
+    item_creation = models.ItemCreate(
+        description=description,
+        price_in_cents=price_in_cents,
+        owner_id=seller.user_id,
+        recipient_id=seller.user_id,
+        sales_event_id=sales_event.sales_event_id,
+    )
+
+    orm_item = session.create_item(item=item_creation)
+    session.delete_item_by_id(orm_item.item_id)
+
+    items = session.list_items()
+
+    assert len(items) == 0
