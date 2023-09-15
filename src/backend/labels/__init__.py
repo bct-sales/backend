f24efd2f904f70b3f30987135f36195d64a1ee87
Frederic Vogels <frederic.vogels+github@gmail.com>
2023-09-15 16:50:12 +0200

    labels

--- src/backend/labels/__init__.py
+++ src/backend/labels/__init__.py
@@ -44,11 +44,8 @@ def generate_qr_data_for_item(item: Item) -> str:
 
 
 def generate_labels(sheet_specifications: SheetSpecifications, items: list[Item]) -> str:
-    labels_data = [
-        generate_label_data_for_item(item)
-        for item in items
-    ]
-    label_generation_data = LabelGenerationData(labels=labels_data, sheet_specs=sheet_specifications)
+    label_generation_data = build_label_generation_data(sheet_specifications, items)
+
     unique_id = uuid.uuid4()
     output_filename = f'{unique_id}.pdf'
     input_path = f'g:/temp/{unique_id}.json'
@@ -57,7 +54,23 @@ def generate_labels(sheet_specifications: SheetSpecifications, items: list[Item]
     with open(input_path, 'w') as f:
         f.write(label_generation_data.model_dump_json())
 
+    call_qr_generation_subprocess(input_path, output_path)
+
+    return output_filename
+
+
+def build_label_generation_data(sheet_specifications: SheetSpecifications, items: list[Item]) -> LabelGenerationData:
+    labels_data = [
+        generate_label_data_for_item(item)
+        for item in items
+    ]
+    return LabelGenerationData(labels=labels_data, sheet_specs=sheet_specifications)
+
+
+def call_qr_generation_subprocess(input_path: str, output_path: str):
     environment = os.environ.copy()
+
+    # Needs to be removed otherwise subprocess-poetry will fail to set up correct environment
     if 'VIRTUAL_ENV' in environment:
         del environment['VIRTUAL_ENV']
 
@@ -69,5 +82,3 @@ def generate_labels(sheet_specifications: SheetSpecifications, items: list[Item]
         input_path,
         output_path
     ], cwd=r'G:/repos/bct/bctqr', shell=False, env=environment)
-
-    return output_filename
