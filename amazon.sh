9935e62bf3a9346360edc4752142bfd8ca52704b
Frederic Vogels <frederic.vogels+github@gmail.com>
2023-09-23 13:24:45 +0200

    updated amazon scripts

--- amazon.sh
+++ amazon.sh
@@ -1,10 +1,4 @@
 #! /usr/bin/env bash
 
-export BCT_DATABASE_PATH=~/bct.db
-export BCT_HTML_PATH=~/index.html
-export BCT_LABEL_GENERATION_DIRECTORY=~/labels
-
-mkdir -p $BCT_LABEL_GENERATION_DIRECTORY
-
 cd src
 poetry run uvicorn backend.app:app --reload
--- src/amadmin.sh
+++ src/amadmin.sh
@@ -1,7 +1,3 @@
 #! /usr/bin/env bash
 
-export BCT_DATABASE_PATH=~/bct.db
-export BCT_HTML_PATH=~/index.html
-export BCT_LABEL_GENERATION_DIRECTORY=~/labels
-
 poetry run python admin.py $@
