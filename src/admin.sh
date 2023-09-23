8c79fb43beec9a6af0e8a5de40461c20a782c2c6
Frederic Vogels <frederic.vogels+github@gmail.com>
2023-09-23 13:05:18 +0200

    fixed admin.sh

--- src/admin.sh
+++ src/admin.sh
@@ -2,5 +2,6 @@
 
 export BCT_DATABASE_PATH=G:/repos/bct/backend/bct.db
 export BCT_HTML_PATH=G:/repos/bct/frontend/dist/index.html
+export BCT_LABEL_GENERATION_DIRECTORY=G:/temp
 
 poetry run py admin.py $@
