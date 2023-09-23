b26c110982b87fcb685cc440f2af15d9e5881008
Frederic Vogels <frederic.vogels+github@gmail.com>
2023-09-23 13:13:09 +0200

    moved environment variables to .env

--- src/admin.sh
+++ src/admin.sh
@@ -1,7 +1,3 @@
 #! /usr/bin/env bash
 
-export BCT_DATABASE_PATH=G:/repos/bct/backend/bct.db
-export BCT_HTML_PATH=G:/repos/bct/frontend/dist/index.html
-export BCT_LABEL_GENERATION_DIRECTORY=G:/temp
-
 poetry run py admin.py $@
--- start.sh
+++ start.sh
@@ -1,8 +1,4 @@
 #! /usr/bin/env bash
 
-export BCT_DATABASE_PATH=G:/repos/bct/backend/bct.db
-export BCT_HTML_PATH=G:/repos/bct/frontend/dist/index.html
-export BCT_LABEL_GENERATION_DIRECTORY=G:/temp
-
 cd src
 poetry run uvicorn backend.app:app --reload
