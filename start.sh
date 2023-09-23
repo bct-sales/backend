bef38e5e45bfe0849a8a201423e729c719dba8e1
Frederic Vogels <frederic.vogels+github@gmail.com>
2023-09-23 13:03:29 +0200

    label gen dir env var

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 
 export BCT_DATABASE_PATH=G:/repos/bct/backend/bct.db
 export BCT_HTML_PATH=G:/repos/bct/frontend/dist/index.html
+export BCT_LABEL_GENERATION_DIRECTORY=G:/temp
 
 cd src
 poetry run uvicorn backend.app:app --reload
