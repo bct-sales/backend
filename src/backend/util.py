a4059d979f4139094e39469f05e01f2d2a1d1db9
Frederic Vogels <frederic.vogels+github@gmail.com>
2023-09-24 14:44:37 +0200

    changed url_for

--- src/backend/util.py
+++ src/backend/util.py
@@ -2,6 +2,7 @@ from fastapi import Request
 
 
 def url_for(request: Request, name: str, **kwargs) -> str:
-    # TODO Ugly solution for tricky issue
     url = str(request.url_for(name, **kwargs))
-    return url.replace('http://127.0.0.1:8000', '')
+    # TODO Ugly solution for tricky issue
+    # url = url.replace('http://127.0.0.1:8000', '')
+    return url
