fe9faef12983b99dec8a71c267aeededfb0be4f8
Frederic Vogels <frederic.vogels+github@gmail.com>
2023-09-28 22:22:15 +0200

    update

--- download-html.sh
+++ download-html.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sudo curl -L https://github.com/bct-sales/frontend/releases/latest/download/index.html /var/www/html
-sudo chown www-data:www-data /var/www/html/index.html
+sudo curl -L https://github.com/bct-sales/frontend/releases/latest/download/index.html -o /var/www/html/bct.html
+sudo chown www-data:www-data /var/www/html/bct.html
