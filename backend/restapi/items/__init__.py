c85cdba39edc0004da062b5bdbfc8168f603611c
Frederic Vogels <frederic.vogels+github@gmail.com>
2023-11-02 16:52:31 +0100

    renamed file

--- backend/restapi/items/__init__.py
+++ backend/restapi/items/__init__.py
@@ -3,10 +3,10 @@ from fastapi import APIRouter
 from backend.db.exceptions import *
 
 from . import get
-from . import getall
+from . import listitems
 
 
 router = APIRouter(prefix='/items')
 
 router.include_router(get.router)
-router.include_router(getall.router)
+router.include_router(listitems.router)
