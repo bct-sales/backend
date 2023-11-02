a2506864bb0304ded6379cc4e23322265f0b8561
Frederic Vogels <frederic.vogels+github@gmail.com>
2023-11-02 16:52:38 +0100

    test_get_item

--- tests/rest/rest-item-tests.py
+++ tests/rest/rest-item-tests.py
@@ -19,3 +19,27 @@ def test_list_events_as_cashier(client: TestClient,
     data = response.json()
     assert 'item_ids' in data
     assert len(data['item_ids']) == len(items)
+
+
+def test_get_item(client: TestClient,
+                  session: DatabaseSession,
+                  cashier_headers: dict[str, str],
+                  items: list[models.Item],
+                  items_url: str):
+    for item in items:
+        item_url = f"{items_url}{item.item_id}"
+        response = client.get(item_url, headers=cashier_headers)
+        assert response.status_code == status.HTTP_200_OK
+
+        data = response.json()
+        assert data == {
+            'item_id': item.item_id,
+            'description': item.description,
+            'category': item.category,
+            'price_in_cents': item.price_in_cents,
+            'recipient_id': item.recipient_id,
+            'sales_event_id': item.sales_event_id,
+            'owner_id': item.owner_id,
+            'charity': item.charity,
+            'has_been_sold': False
+        }
