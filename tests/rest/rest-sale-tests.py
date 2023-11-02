9034b4edef8a975a89aba8d36c67760fcf068d8d
Frederic Vogels <frederic.vogels+github@gmail.com>
2023-11-02 21:29:05 +0100

    tests

--- tests/rest/rest-sale-tests.py
+++ tests/rest/rest-sale-tests.py
@@ -11,6 +11,8 @@ from tests.util import Exists
 @pytest.mark.parametrize('sold_item_ids', [
     [1],
     [1, 2],
+    [1, 3, 4],
+    [4, 9, 15, 19]
 ])
 def test_create_sale_as_cashier(client: TestClient,
                                 session: DatabaseSession,
@@ -23,3 +25,29 @@ def test_create_sale_as_cashier(client: TestClient,
     }
     response = client.post(sales_url, json=payload, headers=cashier_headers)
     assert response.status_code == status.HTTP_201_CREATED
+    assert set(sold_item_ids) ==  set(session.collect_sold_items())
+    assert len(sold_item_ids) == len(session.collect_sold_items())
+
+
+def test_create_sale_with_zero_items(client: TestClient,
+                                     session: DatabaseSession,
+                                     cashier_headers: dict[str, str],
+                                     sales_url: str,
+                                     items: list[models.Item]):
+    payload = {
+        'item_ids': [],
+    }
+    response = client.post(sales_url, json=payload, headers=cashier_headers)
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+def test_create_sale_with_duplicate_items(client: TestClient,
+                                          session: DatabaseSession,
+                                          cashier_headers: dict[str, str],
+                                          sales_url: str,
+                                          items: list[models.Item]):
+    payload = {
+        'item_ids': [items[0].item_id, items[0].item_id],
+    }
+    response = client.post(sales_url, json=payload, headers=cashier_headers)
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
