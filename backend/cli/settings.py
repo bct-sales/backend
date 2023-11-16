cd3875fa1cdc4d91ae05e59cd412bb92453cda60
Frederic Vogels <frederic.vogels@kuleuven.be>
2023-11-16 13:19:17 +0100

    settings.py

--- backend/cli/settings.py
+++ backend/cli/settings.py
@@ -11,5 +11,7 @@ def gendotenv():
     with open('.env', 'w') as file:
         print("BCT_JWT_SECRET_KEY=", file=file)
         print("BCT_DATABASE_PATH=", file=file)
+        print("BCT_DB_BACKUP_DIRECTORY=", file=file)
         print("BCT_LABEL_GENERATION_DIRECTORY=", file=file)
         print("BCT_QR_DIRECTORY=", file=file)
+        
